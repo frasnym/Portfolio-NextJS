3949379ce8e481dc852635d98edb6dfd36327d6e
frasnym <frastyawan.nym@gmail.com>
2021-02-23 13:11:26 +0800

    feat(index): Convert to .ts and implement components

---
+++ pages/index.tsx
@@ -0,0 +1,35 @@
+import Navigations from '../components/home-page/navigations/Navigations';
+import SocialLinks from '../components/home-page/social-links/SocialLinks';
+import Layout from '../components/layout/Layout';
+
+export default function Home() {
+	return (
+		<Layout title="Portfolio of FrasNym">
+			<img
+				className="rounded-full max-w-xs max-h-xs mb-5"
+				src="/images/avatar.jpg"
+				alt="Photos of FrasNym"
+			/>
+			<article className="text-gray-300 text-left bg-gray-900 bg-opacity-90 p-5 rounded-3xl">
+				<h1 className="text-white text-6xl">Hi, I'm Fras</h1>
+				<hr className="my-4" />
+				<p className="text-3xl mb-3">
+					A <strong>Full-Stack Developer</strong> from Indonesia{' '}
+					<img
+						className="w-10 inline"
+						src="images/flag-indonesia_1f1ee-1f1e9.png"
+						alt="Flag of Indonesia Country"
+					/>
+				</p>
+				<p className="text-2xl mb-3">
+					Welcome to my portfolio website. Get informed, collaborate
+					and discover projects I was working on through the years!
+				</p>
+				<section className="mt-8">
+					<SocialLinks />
+				</section>
+			</article>
+			<Navigations />
+		</Layout>
+	);
+}
