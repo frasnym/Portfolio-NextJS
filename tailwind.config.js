7c3d5f0163d3cadbcdec2b86bd6308ce3390a4ff
frasnym <frastyawan.nym@gmail.com>
2021-02-23 12:00:12 +0800

    feat(tailwind): Add config for backgroun image

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
 	],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: (theme) => ({
+				'main-bg': "url('/images/background.jpg')",
+			}),
+		},
 	},
 	variants: {
 		extend: {},
